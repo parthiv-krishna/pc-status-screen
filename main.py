af22b314ee7f06fd81598f0992f1781188c12ef4
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2024-08-25 19:55:56 -0700

    main.py progress: turn on display, setup temp sensor, poll from serial

--- main.py
+++ main.py
@@ -1,20 +1,64 @@
-import time
+import adafruit_adt7410
 import board
 import busio
-import adafruit_adt7410
+import displayio
+import json
+import supervisor
+import time
 
-# rate at which to refresh the pyportal screen, in seconds
-PYPORTAL_REFRESH = 2
-board.DISPLAY.rotation = 90
-board.DISPLAY.brightness = 1
+CONFIG_PATH = "./main_config.json"
 
-# init. adt7410
-i2c_bus = busio.I2C(board.SCL, board.SDA)
-adt = adafruit_adt7410.ADT7410(i2c_bus, address=0x48)
-adt.high_resolution = True
+# onboard temperature sensor for case air temp
+def airtemp_init():
+    i2c_bus = busio.I2C(board.SCL, board.SDA)
+    adt = adafruit_adt7410.ADT7410(i2c_bus, address=0x48)
+    adt.high_resolution = True
+    return adt
 
-while True:
-    # read the temperature sensor
-    temperature = adt.temperature
-    print(temperature)
-    time.sleep(PYPORTAL_REFRESH)
+def display_on(display):
+    display.brightness = 1
+
+def display_off(display):
+    display.brightness = 0
+
+def display_init(config):
+    display = board.DISPLAY
+    display.rotation = config["display_ROTATION"]
+
+    display_on(display)
+
+    return board.DISPLAY
+
+def display_update(display, stats, config):
+    display_on(display)
+
+def main(config):
+    display = display_init(config["CPU_CORES"])
+    airtemp_sensor = init_airtemp()
+
+    last_update = time.time()
+    while True:
+        curr_time = time.time()
+
+        if supervisor.runtime.serial_bytes_available:
+            # receive data from serial
+            stats_bytes = input().strip()
+            print(f"Received: {value}\n")
+            stats = json.loads(stats)
+
+            # read the temperature sensor
+            stats["TEMP_AIR"] = airtemp_sensor.temperature
+
+            display_update(display, stats, config)
+
+            last_update = curr_time
+
+        if (curr_time - last_update).total_seconds > config["TIMEOUT_SECONDS"]:
+            # timeout: haven't heard from driver in a while
+            display_off(display)
+
+if __name__ == "__main__":
+    with open(CONFIG_PATH, 'r') as f:
+        config = json.load(f)
+
+    main(config)
